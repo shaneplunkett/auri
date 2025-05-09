c256483e486e02a939abd96215f15b4ed8682950
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2025-05-09 23:57:00 +1000

    Init

---
+++ README.md
@@ -0,0 +1,17 @@
+# README
+
+## About
+
+An LLM client built in Wails
+
+Intent is to allow for:
+
+- Multiple LLM config with a baseline of ChatGPT, Gemini, Claude
+- Adding of API keys
+- Support for custom endpoints
+- MCP server support
+- Theming
+- VIM Support
+- *hopefully* package support for Nix Home-Manager
+
+
---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "A Nix-flake-based Go 1.22 development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+
+  outputs =
+    inputs:
+    let
+      goVersion = 23; # Change this to update the whole stack
+
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [ inputs.self.overlays.default ];
+            };
+          }
+        );
+    in
+    {
+      overlays.default = final: prev: {
+        go = final."go_1_${toString goVersion}";
+      };
+
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # go (version is specified by overlay)
+              go
+              wails
+              upx
+              gtk3
+              pkg-config
+              webkitgtk_4_0
+              # goimports, godoc, etc.
+              gotools
+
+              # https://github.com/golangci/golangci-lint
+              golangci-lint
+            ];
+          };
+        }
+      );
+    };
+}
