6d3a20a81c8a3188fe8af42519b598a599dacb82
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2026-01-12 17:42:37 +1100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <p>hi its me, Auri</p>;
+  return;
 }
--- components/SidebarButton.tsx
+++ components/SidebarButton.tsx
@@ -7,7 +7,7 @@ export default function SidebarButton({ icon, label }: Button) {
   return (
     <div>
       <button className="flex items-center gap-3 rounded-lg px-3 py-2 text-text-secondary-light dark:text-text-secondary-dark hover:bg-primary-subtle dark:hover:bg-primary-subtle-dark hover:text-text-primary-light dark:hover:text-white transition-all hover:shadow-sm dark:hover:shadow-btn-dark">
-        <span className="material-symbols-outlined">{icon}</span>{" "}
+        <span className="material-symbols-outlined">{icon}</span>
         <p className="text-sm font-medium">{label}</p>
       </button>
     </div>
